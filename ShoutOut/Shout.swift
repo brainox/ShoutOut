25adb7346760753dba8e39347c60083c1b0118b7
Obinna <obinna.aguwa@gmail.com>
2022-10-23 16:16:57 +0100

    Implemented NSManaged subclass

---
+++ ShoutOut/Shout.swift
@@ -0,0 +1,17 @@
+//
+//  Shout.swift
+//  ShoutOut
+//
+//  Created by Obinna Aguwa on 23/10/2022.
+//  Copyright © 2022 pluralsight. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Shout: NSManagedObject {
+    @NSManaged var from: String?
+    @NSManaged var message: String?
+    @NSManaged var sentDate: Date?
+    @NSManaged var shoutCategory: String
+}
